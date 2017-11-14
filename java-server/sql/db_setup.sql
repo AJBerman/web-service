b8fcde9434af9b76b0ebd138c6ec3851c65afef9
Davis <davislouie90@gmail.com>
2017-11-13 18:10:08 -0800

    Added x and y. Auto-dent changed a lot in composite.java

--- java-server/sql/db_setup.sql
+++ java-server/sql/db_setup.sql
@@ -1,18 +1,17 @@
-
 CREATE DATABASE IF NOT EXISTS directstem;
 
 USE directstem;
 
 /* USERS */
-DROP TABLE IF EXISTS  users;
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-	id        INTEGER AUTO_INCREMENT NOT NULL,
-	firstName VARCHAR(255) NOT NULL,
-	lastName  VARCHAR(255) NOT NULL,
-	password  VARCHAR(255) NOT NULL,
-	email     VARCHAR(255) NOT NULL UNIQUE,
-	description TEXT NULL,
-	PRIMARY KEY (id)
+  id          INTEGER AUTO_INCREMENT NOT NULL,
+  firstName   VARCHAR(255)           NOT NULL,
+  lastName    VARCHAR(255)           NOT NULL,
+  password    VARCHAR(255)           NOT NULL,
+  email       VARCHAR(255)           NOT NULL UNIQUE,
+  description TEXT                   NULL,
+  PRIMARY KEY (id)
 );
 
 INSERT INTO users (firstName, lastName, password, email)
@@ -26,48 +25,50 @@ VALUES ('Ervin', 'Howell', 'ervin', 'ervin@howell.com');
 
 
 CREATE TABLE nodes (
-	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	description varchar(255),
-	composition boolean,
-	service int,
-	compedge int,
-	outp varchar(255)
+  id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  description VARCHAR(255),
+  composition BOOLEAN,
+  service     INT,
+  compedge    INT,
+  outp        VARCHAR(255),
+  xPos        INT,
+  yPos        INT
 );
 CREATE TABLE services (
-	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	url varchar(255),
-	title varchar(255),
-	description varchar(255),
-	ret varchar(255)
+  id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  url         VARCHAR(255),
+  title       VARCHAR(255),
+  description VARCHAR(255),
+  ret         VARCHAR(255)
 );
 CREATE TABLE serviceparameters (
-	id int NOT NULL,
-	i int,
-	val varchar(255)
+  id  INT NOT NULL,
+  i   INT,
+  val VARCHAR(255)
 );
 CREATE TABLE children (
-	id int NOT NULL,
-	childid int NOT NULL
+  id      INT NOT NULL,
+  childid INT NOT NULL
 );
 CREATE TABLE edges (
-	id int NOT NULL,
-	srcid int NOT NULL,
-	paramNo int
+  id      INT NOT NULL,
+  srcid   INT NOT NULL,
+  paramNo INT
 );
 CREATE TABLE childedges (
-	id int NOT NULL,
-	destid int NOT NULL,
-	paramNo int,
-	inputNo int
+  id      INT NOT NULL,
+  destid  INT NOT NULL,
+  paramNo INT,
+  inputNo INT
 );
 CREATE TABLE inputs (
-	id int NOT NULL,
-	i int,
-	val varchar(255)
+  id  INT NOT NULL,
+  i   INT,
+  val VARCHAR(255)
 );
 CREATE TABLE results (
-	id int NOT NULL,
-	guid varchar(255),
-	val varchar(255),
-	failed boolean
+  id     INT NOT NULL,
+  guid   VARCHAR(255),
+  val    VARCHAR(255),
+  failed BOOLEAN
 );
